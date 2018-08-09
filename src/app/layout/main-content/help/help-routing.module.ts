0278ec3b716d32388b01d65302a53f21e9a7673b
zhonghua-wang <zhonghua.wang@outlook.com>
2018-08-09 18:17:12 +0800

    init help module

--- src/app/layout/main-content/help/help-routing.module.ts
+++ src/app/layout/main-content/help/help-routing.module.ts
@@ -1,13 +1,44 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HelpComponent} from './help/help.component';
+import {OverviewComponent} from './overview/overview.component';
+import {TargetHelpComponent} from './target-help/target-help.component';
+import {MoleculeHelpComponent} from './molecule-help/molecule-help.component';
+import {AssayHelpComponent} from './assay-help/assay-help.component';
 
 const routes: Routes = [
-  {path: '', component: HelpComponent}
+  {
+    path: '',
+    component: HelpComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'overview',
+        pathMatch: 'full'
+      },
+      {
+        path: 'overview',
+        component: OverviewComponent
+      },
+      {
+        path: 'target',
+        component: TargetHelpComponent
+      },
+      {
+        path: 'molecule',
+        component: MoleculeHelpComponent
+      },
+      {
+        path: 'assay',
+        component: AssayHelpComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HelpRoutingModule { }
+export class HelpRoutingModule {
+}
--- src/app/layout/main-content/help/help.module.ts
+++ src/app/layout/main-content/help/help.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HelpRoutingModule } from './help-routing.module';
 import {HelpComponent} from './help/help.component';
 import {SharedModule} from '../../../shared';
+import { OverviewComponent } from './overview/overview.component';
+import { TargetHelpComponent } from './target-help/target-help.component';
+import { MoleculeHelpComponent } from './molecule-help/molecule-help.component';
+import { AssayHelpComponent } from './assay-help/assay-help.component';
 
 @NgModule({
   imports: [
@@ -11,6 +15,6 @@ import {SharedModule} from '../../../shared';
     HelpRoutingModule,
     SharedModule
   ],
-  declarations: [HelpComponent]
+  declarations: [HelpComponent, OverviewComponent, TargetHelpComponent, MoleculeHelpComponent, AssayHelpComponent]
 })
 export class HelpModule { }
